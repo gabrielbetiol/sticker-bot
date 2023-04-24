a926e37be9a9ab542911dc4f43ad5ecb55cad358
gabrielbetiol <gabriel.betiol@gmail.com>
2023-04-24 19:33:20 -0300

    refactor

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ client.on('ready', () => {
     console.log('Client ready');
 });
 
-client.on('message_create', (message) => {
+client.on('message_create', (message: Message) => {
     if (message.body === 'hello') {
         message.reply('world');
     }
@@ -32,70 +32,112 @@ client.on('message_create', (message) => {
     const command: string = message.body.split(' ')[0];
     const sender: any = message.from.includes("85317726") ? message.to : message.from;
     if (command === ".sticker") {
-        generateSticker(message, sender);
+        if (message.hasMedia) {
+            // generateSticker(message, sender);
+            generateSticker(message);
+        } else {
+            message.reply('send image or short video with caption *.sticker*');
+        }
     }
 });
 
 
 client.initialize();
 
-const generateSticker = async (msg: Message, sender: string) => {
-    
-    console.debug(msg.type);
-    if(msg.type === MessageTypes.IMAGE) {
-        try {
-            const { data } = await msg.downloadMedia();
-            const image = await new MessageMedia("image/jpeg", data, "image.jpg");
-            await client.sendMessage(sender, image, { sendMediaAsSticker: true });
-        } catch(e) {
-            msg.reply("❌ Erro ao processar imagem");
-        }
-    } else if (msg.type === MessageTypes.VIDEO) {
-        try {
-            msg.downloadMedia().then(media => {
-                
-                if (media) {
-                    const mediaPath = './downloaded-media/';
+// const generateSticker = async (msg: Message, sender: string) => {
+const generateSticker = async (msg: Message) => {
 
-                    if (!fs.existsSync(mediaPath)) {
-                        fs.mkdirSync(mediaPath);
-                    }
-                    
-                    const extension = mime.extension(media.mimetype);
-                    const fileName = new Date().getTime();
-                    const fullFileName = mediaPath + fileName + '.' + extension;
-                    
-                    try {
-                        fs.writeFileSync(fullFileName, media.data, { encoding: 'base64' });
-                        console.log('File downloaded successfully!', fullFileName);
-                        console.log(fullFileName);
-                        MessageMedia.fromFilePath(fullFileName);
-                        client.sendMessage(msg.from, new MessageMedia(media.mimetype, media.data, fileName), { sendMediaAsSticker: true });
-                        fs.unlinkSync(fullFileName);
-                        console.log(`File Deleted successfully!`, );
-                    } catch (err) {
-                        console.log('Failed to save the file: ', err);
-                        console.log(`File Deleted Successfully!`, );
-                    }
+    try {
+        msg.downloadMedia().then(media => {
+            
+            if (media) {
+                const mediaPath = './downloaded-media/';
+
+                if (!fs.existsSync(mediaPath)) {
+                    fs.mkdirSync(mediaPath);
                 }
-            })
-            // const { data } = await msg.downloadMedia();
-            // const webpVideo = await Util.formatVideoToWebpSticker(data);
-            // await client.sendMessage(sender, webpVideo, { sendMediaAsSticker: true });
-        } catch (e) {
-            console.error(e);
-            msg.reply("❌ Erro ao processar video");
-        }
-    } else {
-        try {
-            const url = msg.body.substring(msg.body.indexOf(" ")).trim();
-            const { data } = await axios.get(url, {responseType: 'arraybuffer'});
-            const returnedB64 = Buffer.from(data).toString('base64');
-            const image = await new MessageMedia("image/jpeg", returnedB64, "image.jpg");
-            await client.sendMessage(sender, image, { sendMediaAsSticker: true });
-        } catch(e) {
-            msg.reply("❌ Não foi possível gerar um sticker com esse link");
-        }
+                
+                const extension = mime.extension(media.mimetype);
+                const fileName = new Date().getTime();
+                const fullFileName = mediaPath + fileName + '.' + extension;
+                
+                try {
+                    fs.writeFileSync(fullFileName, media.data, { encoding: 'base64' });
+                    console.log('File downloaded successfully!', fullFileName);
+                    console.log(fullFileName);
+                    MessageMedia.fromFilePath(fullFileName);
+                    client.sendMessage(msg.from, new MessageMedia(media.mimetype, media.data, fileName), { sendMediaAsSticker: true });
+                    fs.unlinkSync(fullFileName);
+                    console.log(`File Deleted successfully!`, );
+                } catch (err) {
+                    console.log('Failed to save the file: ', err);
+                    console.log(`File Deleted Successfully!`, );
+                }
+            }
+        })
+    } catch (e) {
+        console.error(e);
+        msg.reply("Error to process media to sticker");
     }
+    
+    // console.debug(msg.type);
+    // if(msg.type === MessageTypes.IMAGE) {
+    //     try {
+    //         const { data } = await msg.downloadMedia();
+    //         const image = await new MessageMedia("image/jpeg", data, "image.jpg");
+    //         await client.sendMessage(sender, image, { sendMediaAsSticker: true });
+    //     } catch(e) {
+    //         msg.reply("❌ Erro ao processar imagem");
+    //     }
+    // } else if (msg.type === MessageTypes.VIDEO) {
+    //     try {
+    //         msg.downloadMedia().then(media => {
+                
+    //             if (media) {
+    //                 const mediaPath = './downloaded-media/';
+
+    //                 if (!fs.existsSync(mediaPath)) {
+    //                     fs.mkdirSync(mediaPath);
+    //                 }
+                    
+    //                 const extension = mime.extension(media.mimetype);
+    //                 const fileName = new Date().getTime();
+    //                 const fullFileName = mediaPath + fileName + '.' + extension;
+                    
+    //                 try {
+    //                     fs.writeFileSync(fullFileName, media.data, { encoding: 'base64' });
+    //                     console.log('File downloaded successfully!', fullFileName);
+    //                     console.log(fullFileName);
+    //                     MessageMedia.fromFilePath(fullFileName);
+    //                     client.sendMessage(msg.from, new MessageMedia(media.mimetype, media.data, fileName), { sendMediaAsSticker: true });
+    //                     fs.unlinkSync(fullFileName);
+    //                     console.log(`File Deleted successfully!`, );
+    //                 } catch (err) {
+    //                     console.log('Failed to save the file: ', err);
+    //                     console.log(`File Deleted Successfully!`, );
+    //                 }
+    //             }
+    //         })
+    //         // const { data } = await msg.downloadMedia();
+    //         // const webpVideo = await Util.formatVideoToWebpSticker(data);
+    //         // await client.sendMessage(sender, webpVideo, { sendMediaAsSticker: true });
+    //     } catch (e) {
+    //         console.error(e);
+    //         msg.reply("❌ Erro ao processar video");
+    //     }
+    // } else {
+    //     try {
+    //         const url = msg.body.substring(msg.body.indexOf(" ")).trim();
+    //         const { data } = await axios.get(url, {responseType: 'arraybuffer'});
+    //         const returnedB64 = Buffer.from(data).toString('base64');
+    //         const image = await new MessageMedia("image/jpeg", returnedB64, "image.jpg");
+    //         await client.sendMessage(sender, image, { sendMediaAsSticker: true });
+    //     } catch(e) {
+    //         msg.reply("❌ Não foi possível gerar um sticker com esse link");
+    //     }
+    // }
 }
   
+
+//TODO verificar como salvar sessao
+//Associar a um numero
